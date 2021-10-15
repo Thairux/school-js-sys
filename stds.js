4bbce215b47c4f3bedbe7ad5bf4afa96b69ceaba
Peter Thairu Muigai <ptmthairu@gmail.com>
2021-10-15 11:12:12 +0300

    all credit goes to PTM no idea about readme??

---
+++ stds.js
@@ -0,0 +1,8 @@
+class studentfunction  {
+     functionselector = document.getElementsByClassName('student.age');
+    id = random(int);
+    int = range(1, 9);
+
+    id = random(int);
+    int = range(1, 9);
+};
