658e47c1fc902bd240532c3a1d8fbed863c59777
Thairux <ptmthairu@gmail.com>
2022-12-11 15:59:13 +0300

    deleted useless js file

--- stds.js
+++
@@ -1,8 +0,0 @@
-class studentfunction  {
-     functionselector = document.getElementsByClassName('student.age');
-    id = random(int);
-    int = range(1, 9);
-
-    id = random(int);
-    int = range(1, 9);
-};
